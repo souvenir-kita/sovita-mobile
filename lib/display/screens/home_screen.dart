fd96124de0a23bb20592feaa267e613169bca63d
arzakaraffan <arzakaraffan@gmail.com>
2024-12-02 16:18:09 +0700

    feat: HomeScreen Init

---
+++ lib/display/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Sovita Mobile",
+            style: TextStyle(color: Colors.black,
+            fontWeight: FontWeight.bold,),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
