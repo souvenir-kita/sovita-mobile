833b0a12f47256814c7b0963d8c9007358a2dc37
arzakaraffan <arzakaraffan@gmail.com>
2024-12-04 15:59:17 +0700

    feat: HomeScreen Init

---
+++ lib/adminview/screens/adminmain.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
