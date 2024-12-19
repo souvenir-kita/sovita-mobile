f25e8df81eb5e46216e23c53892d1f5dffd44027
rafliesa <muhammadrafli1579@gmail.com>
2024-12-19 20:19:03 +0700

    changes: mengubah expaded menjadi singlechildscrollview untuk fyp

--- lib/display/screens/fyp.dart
+++ lib/display/screens/fyp.dart
@@ -19,31 +19,23 @@ class ForYouPage extends StatelessWidget {
     // Daftar objek GridItemModel dengan value yang lebih lengkap
     List<GridItemModel> items = [
       GridItemModel(label: "FnB", value: 1, icon: "lib/icons/fnb.png"),
-      GridItemModel(
-          label: "Dekorasi", value: 2, icon: "lib/icons/dekorasi.png"),
-      GridItemModel(
-          label: "Aksesoris", value: 3, icon: "lib/icons/aksesoris.png"),
+      GridItemModel(label: "Dekorasi", value: 2, icon: "lib/icons/dekorasi.png"),
+      GridItemModel(label: "Aksesoris", value: 3, icon: "lib/icons/aksesoris.png"),
       GridItemModel(label: "Pakaian", value: 4, icon: "lib/icons/pakaian.png"),
-      GridItemModel(
-          label: "Bodycare", value: 5, icon: "lib/icons/aromaterapi.png"),
+      GridItemModel(label: "Bodycare", value: 5, icon: "lib/icons/aromaterapi.png"),
       GridItemModel(label: "Semua", value: 6, icon: "lib/icons/all.png"),
     ];
 
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Color(0xFFF09027),
-      //   title: Image.asset('lib/assets/title.png', width: 100),
-      //   centerTitle: true,
-      // ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFFF09027), Color(0xFF8CBEAA)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: SingleChildScrollView(  // Wrap the entire content with SingleChildScrollView
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFF09027), Color(0xFF8CBEAA)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Center(
           child: Column(
             children: [
               const SizedBox(height: 10),
@@ -76,70 +68,69 @@ class ForYouPage extends StatelessWidget {
                   ),
                 ),
               ),
-              Expanded(
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Color(0x80D9D9D9),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: MediaQuery.of(context).size.height * 0.35,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(12, 10, 0, 0),
-                        child: Text(
-                          "Produk Populer",
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
+              SizedBox(height: 20),  // Added some space for separation
+              Container(
+                decoration: const BoxDecoration(
+                  color: Color(0x80D9D9D9),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(12, 10, 0, 0),
+                      child: Text(
+                        "Produk Populer",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
                       ),
-                      Expanded(
-                        child: FutureBuilder<List<Product>>(
-                          future: fetchProductRandom(request),
-                          builder: (context, AsyncSnapshot snapshot) {
-                            if (snapshot.data == null) {
-                              return const Center(
-                                  child: CircularProgressIndicator());
-                            } else {
-                              return GridView.builder(
-                                gridDelegate:
-                                    const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  crossAxisSpacing: 10.0,
-                                  mainAxisSpacing: 10.0,
-                                  childAspectRatio: 3 / 4,
-                                ),
-                                itemCount: snapshot.data!.length,
-                                itemBuilder: (_, index) {
-                                  return InkWell(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              ProductDetailPage(
-                                            product: snapshot.data![index],
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                    borderRadius: BorderRadius.circular(15),
-                                    child: ProductCard(
-                                        product: snapshot.data![index]),
+                    ),
+                    SizedBox(height: 10),  // Added space between the title and the grid
+                    FutureBuilder<List<Product>>(
+                      future: fetchProductRandom(request),
+                      builder: (context, AsyncSnapshot snapshot) {
+                        if (snapshot.data == null) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else {
+                          return GridView.builder(
+                            shrinkWrap: true,  // Makes the grid only take up as much space as needed
+                            physics: NeverScrollableScrollPhysics(),  // Disables scrolling on the grid
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 10.0,
+                              mainAxisSpacing: 10.0,
+                              childAspectRatio: 3 / 4,
+                            ),
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (_, index) {
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          ProductDetailPage(
+                                        product: snapshot.data![index],
+                                      ),
+                                    ),
                                   );
                                 },
+                                borderRadius: BorderRadius.circular(15),
+                                child: ProductCard(
+                                    product: snapshot.data![index]),
                               );
-                            }
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
+                            },
+                          );
+                        }
+                      },
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -167,19 +158,19 @@ class GridItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        if(item.value == 1){
+        if (item.value == 1) {
           Navigator.push(context, MaterialPageRoute(builder: (context) => const CategoryProductPage(categoryValue: 1)));
         }
-        if(item.value == 2){
+        if (item.value == 2) {
           Navigator.push(context, MaterialPageRoute(builder: (context) => const CategoryProductPage(categoryValue: 2)));
         }
-        if(item.value == 3){
+        if (item.value == 3) {
           Navigator.push(context, MaterialPageRoute(builder: (context) => const CategoryProductPage(categoryValue: 3)));
         }
-        if(item.value == 4){
+        if (item.value == 4) {
           Navigator.push(context, MaterialPageRoute(builder: (context) => const CategoryProductPage(categoryValue: 4)));
         }
-        if(item.value == 5){
+        if (item.value == 5) {
           Navigator.push(context, MaterialPageRoute(builder: (context) => const CategoryProductPage(categoryValue: 5)));
         }
         if (item.value == 6) {
